29b34db265dbe4f31c167a4ef0dd1bc454755829
ngocmunghy <mungnn@mirabo-global.com>
2025-01-20 18:26:18 +0700

    feat: add files

--- Assets/Scripts/BlockInteraction.cs
+++ Assets/Scripts/BlockInteraction.cs
@@ -7,7 +7,6 @@ public class BlockInteraction : MonoBehaviour
 
     Camera cam;
     BlockItems blockItems;
-    public bool isHolding;
 
     Vector3 offsetFromFirstTouch;
     Vector3 offsetFromFirstBlock;
@@ -16,6 +15,7 @@ public class BlockInteraction : MonoBehaviour
 
     Vector3 basePosBlock;
 
+    public bool isHolding;
     public bool canMoveBlock;
 
     // Start is called before the first frame update
@@ -25,89 +25,94 @@ public class BlockInteraction : MonoBehaviour
         blockItems = GetComponent<BlockItems>();
     }
 
-    private void OnEnable()
+    //private void OnEnable()
+    //{
+    //    isHolding = false;
+    //    canMoveBlock = true;
+    //    basePosBlock = transform.position;
+    //}
+
+    //void Update()
+    //{
+
+    //    if (!canMoveBlock)
+    //        return;
+
+    //    if (isHolding)
+    //    {
+    //        var screenPoint = Input.mousePosition;
+    //        screenPoint.z = 10.0f; //distance of the plane from the camera
+    //        transform.position = cam.ScreenToWorldPoint(screenPoint) - offsetFromFirstTouch;
+    //    }
+    //}
+
+    //private void OnMouseDown()
+    //{
+    //    if (!canMoveBlock) return;
+    //    isHolding = true;
+    //    Vector3 lastPos = transform.position;
+    //    var screenPoint = Input.mousePosition;
+    //    screenPoint.z = 10f;
+    //    offsetFromFirstTouch = cam.ScreenToWorldPoint(screenPoint) - lastPos;
+    //}
+
+    //private void OnMouseUpAsButton()
+    //{
+    //    if (!canMoveBlock) return;
+    //    isHolding = false;
+    //    if (CheckValidateBlock())
+    //    {
+    //        transform.position += offsetFromFirstBlock;
+    //        canMoveBlock = false;
+    //    } else
+    //    {
+    //        ResetPos();
+    //    }
+    //}
+
+    public void ResetPos()
     {
-        isHolding = false;
-        canMoveBlock = true;
-        basePosBlock = transform.position;
+        transform.position = basePosBlock;
     }
 
-    void Update()
-    {
+    //bool CheckValidateBlock()
+    //{
+    //    nearestCell = GetNeareastCellByPosition(blockItems.GetFirstItemComp().transform.position);
 
-        if (!canMoveBlock)
-            return;
+    //    if (nearestCell == null || nearestCell.isColored == true)
+    //    {
+    //        return false;
+    //    }
 
-        if (isHolding)
-        {
-            var screenPoint = Input.mousePosition;
-            screenPoint.z = 10.0f; //distance of the plane from the camera
-            transform.position = cam.ScreenToWorldPoint(screenPoint) - offsetFromFirstTouch;
-        }
-    }
-
-    private void OnMouseDown()
-    {
-        if (!canMoveBlock) return;
-        isHolding = true;
-        Vector3 lastPos = transform.position;
-        var screenPoint = Input.mousePosition;
-        screenPoint.z = 10f;
-        offsetFromFirstTouch = cam.ScreenToWorldPoint(screenPoint) - lastPos;
-    }
-
-    private void OnMouseUpAsButton()
-    {
-        if (!canMoveBlock) return;
-        isHolding = false;
-        if (CheckValidateBlock())
-        {
-            transform.position += offsetFromFirstBlock;
-            canMoveBlock = false;
-        } else
-        {
-            transform.position = basePosBlock;
-        }
-    }
-
-    bool CheckValidateBlock()
-    {
-        nearestCell = GetNeareastCellByPosition(blockItems.GetFirstItemComp().transform.position);
-
-        if (nearestCell == null || nearestCell.isColored == true)
-        {
-            return false;
-        }
-
-        BlockComp[] blockItemComps = blockItems.GetBlockItemComps();
+    //    BlockComp[] blockItemComps = blockItems.GetBlockItemComps();
         
-        for (int i = 1; i < blockItemComps.Length; i++)
-        {
-            Cell cellInBoard = GetNeareastCellByPosition(blockItemComps[i].transform.position);
+    //    for (int i = 1; i < blockItemComps.Length; i++)
+    //    {
+    //        Cell cellInBoard = GetNeareastCellByPosition(blockItemComps[i].transform.position);
 
-            if (cellInBoard == null || cellInBoard.isColored == true)
-            {
-                return false;
-            }
-        }
+    //        if (cellInBoard == null || cellInBoard.isColored == true)
+    //        {
+    //            return false;
+    //        }
+    //    }
 
-        return true;
-    }
+    //    return true;
+    //}
 
-    public Cell GetNeareastCellByPosition(Vector3 pos)
-    {
+    //public Cell GetNeareastCellByPosition(Vector3 pos)
+    //{
 
-        float distance = Mathf.Sqrt(2 * 0.463f * 0.463f);
+    //    float distance = Mathf.Sqrt(2 * 0.463f * 0.463f);
 
-        for (int i = 0; i < BoardGame.Instance.cellList.Count; i++)
-        {
-            Cell cell = BoardGame.Instance.cellList[i];
-            if (Vector2.Distance(cell.transform.position, pos) < distance)
-            {
-                offsetFromFirstBlock = cell.transform.position - pos;
-                return cell;
-            }
-        }
-        return null;
-    }
+    //    for (int i = 0; i < BoardGame.Instance.cellList.Count; i++)
+    //    {
+    //        Cell cell = BoardGame.Instance.cellList[i];
+    //        if (Vector2.Distance(cell.transform.position, pos) < distance)
+    //        {
+    //            offsetFromFirstBlock = cell.transform.position - pos;
+    //            return cell;
+    //        }
+    //    }
+    //    return null;
+    //}
 }
--- Assets/Scripts/Blocks/BlockComp.cs
+++ Assets/Scripts/Blocks/BlockComp.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class BlockComp : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public BlockItems blockItems;
+    public BlockInteraction blockInter;
+    public Cell nearestCell;
+    Camera cam;
+    Vector3 offsetFromFirstBlock;
+    Vector3 offsetFromFirstTouch;
+
     void Start()
+    {
+        blockItems = GetComponentInParent<BlockItems>();
+        blockInter = GetComponentInParent<BlockInteraction>();
+        cam = Camera.main;
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        if (!blockInter.canMoveBlock) return;
+        blockInter.isHolding = true;
+        Vector3 lastPos = transform.position;
+        var screenPoint = Input.mousePosition;
+        screenPoint.z = 10f;
+        offsetFromFirstTouch = cam.ScreenToWorldPoint(screenPoint) - lastPos;
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (!blockInter.canMoveBlock) return;
+        blockInter.isHolding = false;
+        if (CheckValidateBlock())
+        {
+            transform.position += offsetFromFirstBlock;
+            blockInter.canMoveBlock = false;
+        }
+        else
+        {
+            blockInter.ResetPos();
+        }
+    }
+
+    bool CheckValidateBlock()
+    {
+        nearestCell = GetNeareastCellByPosition(blockItems.GetFirstItemComp().transform.position);
+
+        if (nearestCell == null || nearestCell.isColored == true)
+        {
+            return false;
+        }
+
+        BlockComp[] blockItemComps = blockItems.GetBlockItemComps();
+
+        for (int i = 1; i < blockItemComps.Length; i++)
+        {
+            Cell cellInBoard = GetNeareastCellByPosition(blockItemComps[i].transform.position);
+
+            if (cellInBoard == null || cellInBoard.isColored == true)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public Cell GetNeareastCellByPosition(Vector3 pos)
+    {
+
+        float distance = Mathf.Sqrt(2 * 0.463f * 0.463f);
+
+        for (int i = 0; i < BoardGame.Instance.cellList.Count; i++)
+        {
+            Cell cell = BoardGame.Instance.cellList[i];
+            if (Vector2.Distance(cell.transform.position, pos) < distance)
+            {
+                offsetFromFirstBlock = cell.transform.position - pos;
+                return cell;
+            }
+        }
+        return null;
     }
 }
